cc2378c927433238190e6fe787fbc52d72f5837f
Ernests Lielaisramans <ernests.lielaisramans@ventspils.edu.lv>
2026-01-21 15:14:50 +0200

    salaboju visu

---
+++ 2024/API.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+response = requests.get("https://restcountries.com/v3.1/all?fields=name,population,area,subregion,borders")
+responseLV = requests.get("https://restcountries.com/v3.1/name/latvija")
+print(response.status_code)
+responseParsed=json.loads(response.text)
+responseParsedLV=json.loads(responseLV.text)
+allPopulation = 0
+amount = 0
+populationMax = -1
+allArea = 0
+subRegionLV =''
+bordersLV = []
+for country in responseParsed:
+    amount += 1
+    allPopulation += country['population']
+    allArea += country['area']
+    if populationMax < country['population']:
+        populationMax = country['population']
+
+for countryLV in responseParsedLV:
+    subRegionLV = countryLV['subregion']
+    bordersLV = countryLV['borders']
+
+avgPop = allPopulation / amount
+
+
+
+print(avgPop)
+print(populationMax)
+print(allArea)
+print(subRegionLV)
+print(bordersLV)
